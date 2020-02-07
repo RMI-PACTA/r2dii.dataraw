b7462aa896eb1f70e3b33b082f1362a7fb9fbcaa
Jackson Hoffart <jackson.hoffart@gmail.com>
2020-02-07 17:17:30 +0100

    updated overwrite_demo to use 2dii ids

--- data-raw/overwrite_demo.R
+++ data-raw/overwrite_demo.R
@@ -2,4 +2,4 @@
 path <- here::here("data-raw/overwrite.csv")
 overwrite_demo <- readr::read_csv(path)
 
-usethis::use_data(overwrite_demo, overwrite = TRUE)
+usethis::use_data(overwrite_demo, overwrite = FALSE)
