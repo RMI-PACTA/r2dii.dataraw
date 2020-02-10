cd10c2fb05446afac44054895f296f929943ed7c
Mauro Lepore <maurolepore@gmail.com>
2020-02-10 10:54:04 -0600

    Update overwrite_demo.R
    I find it more convenient to use `overwrite = TRUE`, and others seem to agree, e.g. <https://github.com/search?q=org%3Atidyverse+use_data&type=Code>.

--- data-raw/overwrite_demo.R
+++ data-raw/overwrite_demo.R
@@ -2,4 +2,4 @@
 path <- here::here("data-raw/overwrite_demo.csv")
 overwrite_demo <- readr::read_csv(path)
 
-usethis::use_data(overwrite_demo, overwrite = FALSE)
+usethis::use_data(overwrite_demo, overwrite = TRUE)
